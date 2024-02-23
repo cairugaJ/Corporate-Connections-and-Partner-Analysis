726de2b8776b2d7b1d203a1403f50e9b0b89e030
João Cairuga <106031854+cairugaJ@users.noreply.github.com>
2024-02-23 11:46:07 -0300

    CREATE insercao.py
    Script para inserir os dados coletados dos ZIPs no banco de dados

---
+++ insercao_RF.py
@@ -0,0 +1,10 @@
+# Bibliotecas
+from bs4 import BeautifulSoup
+import requests
+import os
+import time
+from datetime import datetime
+from tqdm import tqdm
+import shutil
+import mysql.connector
+import zipfile
